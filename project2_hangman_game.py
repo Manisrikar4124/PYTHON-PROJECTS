d88cb7a5f320fc7824033f776cce8f2c0bc6ae44
Mani Srikar Baswaraju <141936831+Manisrikar4124@users.noreply.github.com>
2024-06-17 15:29:49 +0530

    Add files via upload

---
+++ project2_hangman_game.py
@@ -0,0 +1,34 @@
+import random
+word_list=["apple","banana","apricot","blackberry","blueberry","cherry","cranberry","custardapple","dates","dragonfruit","grapes","jackfruit","kiwi","lychee","mango","muskmelon","orange","papaya","peach","pear","pineapple","pomegranate","raspberry","chikoo","strawberry","watermelon"]
+selected_word=random.choice(word_list)
+lives=len(selected_word)+2
+print("WELCOME TO THE HANGMAN GAME !!!")
+print(f"You have {lives} chances to guess the word")
+
+#for the selected word empty blanks are assigned  
+display=[]
+for i in range(len(selected_word)):
+    display += '_'
+print(display)
+
+#logic for the game starts here 
+game_ends=False
+while not game_ends:
+    guessed_letter=input("Guess a letter: ").lower()
+    for position in range(len(selected_word)):
+        letter=selected_word[position]
+        if letter==guessed_letter:
+            display[position]=guessed_letter
+    print(display)            
+                
+    if guessed_letter not in selected_word:
+        lives -= 1
+        print(f"{lives} Chances left")
+        if lives == 0:
+            game_ends = True
+            print("NO MORE CHANCES LEFT")
+            print("         ...GAME OVER...")
+    if '_' not in display:
+            game_ends=True
+            print("*** YOU WON ***")    
+            
